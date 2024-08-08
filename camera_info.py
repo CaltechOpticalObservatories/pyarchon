3c1973dcf909efcb9853541b1ab7af598aacbec3
Mike Langmayr <1809691+mikelangmayr@users.noreply.github.com>
2024-08-08 11:04:48 -0700

    Move Pyarchon to Python 3 (#3)
    * Conversion to python 3, PEP8 compliance
    
    * PEP8 compliance in interface.py
    
    * Move to Python 3 (#1)
    
    * run 2to3 on this repo
    
    * linting to pep8 standard
    
    * Update README.md
    
    * update README.md
    
    ---------
    
    Co-authored-by: Don Neill <neilljd@gmail.com>

--- camera_info.py
+++ camera_info.py
@@ -10,13 +10,12 @@
 # the camera module of the ztf package.
 # --------------------------------------------------------------------------
 class CameraInfo(object):
-
     """
     This is the CameraInfo class, which is used to store
     and retrieve current settings, in particular the
     camera mode, image type, basename, exposure time,
     and compression type.
-    
+
     This is for internal use only by the camera module
     of the ztf package. The user should never need to
     look in here.
@@ -24,45 +23,47 @@ class CameraInfo(object):
 
     # codes for types
     #
-    TYPE = { "OBJECT"       :0,
-             "BIAS"         :1,
-             "DARK"         :2,
-             "DOME_FLAT"    :3,
-             "TWILIGHT_FLAT":4,
-             "FOCUS"        :5,
-             "POINTING"     :6,
-             "TEST"         :7,
-             "ILLUMINATION" :8,
-             "FRINGE"       :9,
-             "SEEING"       :10,
-             "OTHER"        :11 }
-    TYPE_NAME = {v: k for k, v in TYPE.iteritems()}
+    TYPE = {
+        "OBJECT": 0,
+        "BIAS": 1,
+        "DARK": 2,
+        "DOME_FLAT": 3,
+        "TWILIGHT_FLAT": 4,
+        "FOCUS": 5,
+        "POINTING": 6,
+        "TEST": 7,
+        "ILLUMINATION": 8,
+        "FRINGE": 9,
+        "SEEING": 10,
+        "OTHER": 11,
+    }
+    TYPE_NAME = {v: k for k, v in TYPE.items()}
 
     # codes for FITS compression types
     #
-    COMPRESSION = { "NONE":0,
-                    "RICE":1,
-                    "GZIP":2,
-                    "PLIO":3 }
-    COMPRESSION_NAME = {v: k for k, v in COMPRESSION.iteritems()}
+    COMPRESSION = {"NONE": 0, "RICE": 1, "GZIP": 2, "PLIO": 3}
+    COMPRESSION_NAME = {v: k for k, v in COMPRESSION.items()}
 
-    def __init__(self, mode="DEFAULT", \
-                       imtype=TYPE["TEST"],  \
-                       basename="",          \
-                       compression=COMPRESSION["NONE"], \
-                       noisebits=4,          \
-                       iterations=1,         \
-                       exptime=0             ):
+    def __init__(
+        self,
+        mode="DEFAULT",
+        imtype=TYPE["TEST"],
+        basename="",
+        compression=COMPRESSION["NONE"],
+        noisebits=4,
+        iterations=1,
+        exptime=0,
+    ):
         """
         initialize the class
         """
-        self.imtype      = imtype
-        self.mode        = mode
-        self.exptime     = exptime
-        self.iterations  = iterations
+        self.imtype = imtype
+        self.mode = mode
+        self.exptime = exptime
+        self.iterations = iterations
         self.compression = compression
-        self.noisebits   = noisebits
-        self.basename    = basename
+        self.noisebits = noisebits
+        self.basename = basename
 
     def get_basename(self):
         """
@@ -114,21 +115,21 @@ class CameraInfo(object):
             self.imtype = self.TYPE[imtype]
             return 0
         else:
-            print "  valid CameraInfo types:",
-            print self.TYPE.keys()
+            print("  valid CameraInfo types:", end=" ")
+            print(self.TYPE.keys())
             return 1
-        
-    def set_compression(self, type, noisebits):
+
+    def set_compression(self, ctype, noisebits):
         """
         Set the compression type.
         """
-        if type in self.COMPRESSION.keys():
-            self.compression = self.COMPRESSION[type]
-            self.noisebits   = noisebits
+        if ctype in self.COMPRESSION.keys():
+            self.compression = self.COMPRESSION[ctype]
+            self.noisebits = noisebits
             return 0
         else:
-            print "  valid CameraInfo compression types:",
-            print self.COMPRESSION.keys()
+            print("  valid CameraInfo compression types:", end=" ")
+            print(self.COMPRESSION.keys())
             return 1
 
     def set_mode(self, mode_in):
@@ -147,9 +148,9 @@ class CameraInfo(object):
             self.exptime = exptime
             return 0
         else:
-            print "  exptime must be >= 0"
+            print("  exptime must be >= 0")
             return 1
-            
+
     def set_iterations(self, iterations):
         """
         Set the iterations
@@ -158,13 +159,12 @@ class CameraInfo(object):
             self.iterations = iterations
             return 0
         else:
-            print "  iterations must be > 0"
+            print("  iterations must be > 0")
             return 1
-            
+
     def set_basename(self, basename):
         """
         Set the basename.
         """
         self.basename = basename
         return 0
-
